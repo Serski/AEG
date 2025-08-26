60a38c8922e054f8c710dc4010752c6f77e18f02
Serski <159945982+Serski@users.noreply.github.com>
2025-08-26 20:11:09 +0300

    Add initRaidTargets initialization script

---
+++ initRaidTargets.js
@@ -0,0 +1,17 @@
+const dbm = require('./database-manager');
+const fs = require('fs');
+const path = require('path');
+
+async function initRaidTargets() {
+  // Read the JSON file you created under jsonStorage
+  const filePath = path.join(__dirname, 'jsonStorage', 'raidTargets.json');
+  const targets = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+
+  // Save the entire object to the 'raidTargets' collection
+  await dbm.saveCollection('raidTargets', targets);
+  console.log('raidTargets saved to DB');
+}
+
+initRaidTargets().catch((err) => {
+  console.error(err);
+});
