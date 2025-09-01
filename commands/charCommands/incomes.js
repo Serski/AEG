7d4f7891e99db1ae6d312c00a9bdfd94eac87bd0
Serski <159945982+Serski@users.noreply.github.com>
2025-09-01 15:42:14 +0300

    Make /incomes response ephemeral

--- commands/charCommands/incomes.js
+++ commands/charCommands/incomes.js
@@ -5,13 +5,16 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('incomes')
 		.setDescription('Collect your daily incomes'),
-	async execute(interaction) {
-        const userID = interaction.user.tag;
-		const numericID = interaction.user.id;
-		var [replyEmbed, replyString] = await char.incomes(userID, numericID);
-		await interaction.reply(({ embeds: [replyEmbed] }));
-		if (replyString) {
-			interaction.channel.send(replyString);
-		}
-	},
+        async execute(interaction) {
+                const userID = interaction.user.tag;
+                const numericID = interaction.user.id;
+
+                await interaction.deferReply({ ephemeral: true });
+
+                const [replyEmbed, replyString] = await char.incomes(userID, numericID);
+                await interaction.editReply({ embeds: [replyEmbed] });
+                if (replyString) {
+                        await interaction.followUp({ content: replyString, ephemeral: true });
+                }
+        },
 };
