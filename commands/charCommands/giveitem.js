60b69c5c01785db23dcec5e2af8ba849d33e1790
Serski <159945982+Serski@users.noreply.github.com>
2025-10-06 20:38:06 +0300

    Make give commands public replies

--- commands/charCommands/giveitem.js
+++ commands/charCommands/giveitem.js
@@ -11,7 +11,7 @@ module.exports = {
         .addStringOption(option => option.setName('item').setDescription('The item to give').setRequired(true))
         .addIntegerOption(option => option.setName('amount').setDescription('The amount of items to give').setRequired(false)),
     async execute(interaction) {
-            await interaction.deferReply({ flags: 64 });
+        await interaction.deferReply();
         const numericID = interaction.user.id;
         const player = interaction.options.getUser('player').id;
         const item = interaction.options.getString('item');
